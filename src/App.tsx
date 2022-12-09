66ddacdee678e14e45ab3d69928b5b46265245b5
hoona1011 <hoona1011@gmail.com>
2022-12-09 20:12:27 +0900

    design: container 및 inner 레이아웃 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Footer from 'components/Footer';
+import PageTitle from 'components/PageTitle';
+import { useLocation } from 'react-router-dom';
 import Router from 'Router';
 
 function App() {
+  const { pathname: pathName } = useLocation();
+
   return (
-    <>
-      <Router />
+    <div className="w-[100%]  flex justify-center items-center">
+      <div className="w-[50%] bg-gradient-to-b from-[white] to-[#EAF0FF] h-screen">
+        <PageTitle pathName={pathName} />
+        <Router />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
