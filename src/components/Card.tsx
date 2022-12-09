1d15f65b13934b7f0347e02af98391d5dec9053c
hoona1011 <hoona1011@gmail.com>
2022-12-09 15:35:56 +0900

    feat: 카드 컴포넌트 구현

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useAppDispatch } from 'store';
+import { toggleFavorite } from 'store/slices/dustSlice';
+import dustGrader from 'utils/dustGrader';
 
-function Card() {
-  return <div>Card</div>;
+interface CardProps {
+  stationName: string;
+  sidoName: string;
+  pm10Grade: string;
+  pm10Value: string;
+  dataTime: string;
+  isFavorite: boolean;
+}
+function Card({
+  stationName,
+  sidoName,
+  pm10Grade,
+  pm10Value,
+  dataTime,
+  isFavorite,
+}: CardProps) {
+  const dispatch = useAppDispatch();
+
+  const onFavoriteHandler = () => {
+    dispatch(toggleFavorite(stationName));
+  };
+
+  return (
+    <div>
+      <div>{stationName}</div>
+      <div>{sidoName}</div>
+      <div>PM10 등급 : {dustGrader(pm10Grade)}</div>
+      <div>PM10 미세먼지 수치: {pm10Value}</div>
+      <div>({dataTime} 기준)</div>
+      <button type="button" onClick={onFavoriteHandler}>
+        {isFavorite ? '✔✔✔✔✔✔✔✔' : '해제됨'}
+      </button>
+    </div>
+  );
 }
 
 export default Card;
