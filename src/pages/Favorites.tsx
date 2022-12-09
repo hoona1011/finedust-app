f599a2dc318256dc32fe0cdf5698317a77cc93a1
hoona1011 <hoona1011@gmail.com>
2022-12-09 15:36:55 +0900

    feat: 즐겨찾기 페이지 구현

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import Card from 'components/Card';
+import { DustInfo } from 'store/slices/dustSlice';
 
-function Favorites() {
-  return <div>Favorites</div>;
+interface FavoritesProps {
+  dustData: DustInfo[];
+}
+
+function Favorites({ dustData }: FavoritesProps) {
+  const favoritedDustData = dustData.filter((data) => {
+    return data.isFavorite === true;
+  });
+  return (
+    <>
+      <h1>즐겨찾기</h1>
+
+      <div>
+        {favoritedDustData?.map((data) => {
+          const {
+            sidoName,
+            stationName,
+            pm10Grade,
+            pm10Value,
+            dataTime,
+            isFavorite,
+          } = data;
+          return (
+            <Card
+              key={stationName}
+              sidoName={sidoName}
+              stationName={stationName}
+              pm10Grade={pm10Grade}
+              pm10Value={pm10Value}
+              dataTime={dataTime}
+              isFavorite={isFavorite}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Favorites;
