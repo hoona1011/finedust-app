abc55b84598a5da551aa5d29d06962b9aeb5e7e7
hoona1011 <hoona1011@gmail.com>
2022-12-09 20:10:07 +0900

    feat: PageTitle 컴포넌트 기능 구현

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function PageTitle({ pathName }: { pathName: string }) {
+  const pathToTitle = (path: string) => {
+    switch (path) {
+      case '/':
+        return '전체 지역 보기';
+      case '/my-region':
+        return '내 지역 보기';
+      case '/favorites':
+        return '즐겨찾기';
+      default:
+        return '페이지 오류';
+    }
+  };
+  return (
+    <div className="text-[40px] font-[rotobo] font-bold text-center my-[30px]">
+      {pathToTitle(pathName)}
+    </div>
+  );
+}
+
+export default PageTitle;
