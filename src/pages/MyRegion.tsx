c268fe617074fd0606c7703bad2129448dcdf493
hoona1011 <hoona1011@gmail.com>
2022-12-09 15:36:47 +0900

    feat: 내 지역 보기 페이지 구현

--- src/pages/MyRegion.tsx
+++ src/pages/MyRegion.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import Selector from 'components/Selector';
+import React, { useMemo } from 'react';
+import sidoNames from 'constants/sidoNames';
+import Card from 'components/Card';
+import { DustInfo } from 'store/slices/dustSlice';
 
-function MyRegion() {
-  return <div>MyRegion</div>;
+interface MyRegionProps {
+  sido: string;
+  setSido: React.Dispatch<React.SetStateAction<string>>;
+  dustData: DustInfo[];
+  myRegion: string;
+  setMyRegion: React.Dispatch<React.SetStateAction<string>>;
+}
+function MyRegion({
+  sido,
+  setSido,
+  dustData,
+  myRegion,
+  setMyRegion,
+}: MyRegionProps) {
+  const myRegions = useMemo(() => {
+    return dustData.map((data) => {
+      return data.stationName;
+    });
+  }, [dustData]);
+
+  const filteredDustData = useMemo(() => {
+    return dustData.filter((data) => {
+      return data.stationName === myRegion;
+    });
+  }, [dustData, myRegion]);
+
+  return (
+    <>
+      <Selector regions={sidoNames} region={sido} setRegion={setSido} />
+      <Selector regions={myRegions} region={myRegion} setRegion={setMyRegion} />
+
+      <div>
+        {filteredDustData?.map((data) => {
+          const {
+            sidoName,
+            stationName,
+            pm10Grade,
+            pm10Value,
+            dataTime,
+            isFavorite,
+          } = data;
+          return (
+            <Card
+              key={stationName}
+              sidoName={sidoName}
+              stationName={stationName}
+              pm10Grade={pm10Grade}
+              pm10Value={pm10Value}
+              dataTime={dataTime}
+              isFavorite={isFavorite}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default MyRegion;
