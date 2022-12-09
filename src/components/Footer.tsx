86daa7022483ce7fb263a05b6d4795be3e518a66
hoona1011 <hoona1011@gmail.com>
2022-12-09 15:37:20 +0900

    feat: Footer 네비게이트 기능 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AllRegionsIcon, FavoritesIcon, MyRegionIcon } from './Icons';
 
 function Footer() {
   const navigate = useNavigate();
   return (
-    <div>
-      <button type="button" onClick={() => navigate('my-region')}>
+    <div className="flex justify-between sticky top-0">
+      <MyRegionIcon onClick={() => navigate('my-region')} />
+      <AllRegionsIcon onClick={() => navigate('/')} />
+      <FavoritesIcon onClick={() => navigate('/favorites')} />
+      {/* <button type="button" onClick={() => navigate('my-region')}>
         내 지역
       </button>
       <button type="button" onClick={() => navigate('/')}>
@@ -13,7 +17,7 @@ function Footer() {
       </button>
       <button type="button" onClick={() => navigate('/favorites')}>
         즐겨찾기
-      </button>
+      </button> */}
     </div>
   );
 }
