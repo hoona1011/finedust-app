087d60fac7d64df2e9082072cb26d597991b3949
hoona1011 <hoona1011@gmail.com>
2022-12-09 20:11:42 +0900

    design: 즐겨찾기 페이지 디자인 작업

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,42 +1,43 @@
 import Card from 'components/Card';
-import { DustInfo } from 'store/slices/dustSlice';
+import { useAppSelector } from 'store';
 
-interface FavoritesProps {
-  dustData: DustInfo[];
-}
-
-function Favorites({ dustData }: FavoritesProps) {
+function Favorites() {
+  const dustData = useAppSelector((state) => state.dust.dustInfos);
   const favoritedDustData = dustData.filter((data) => {
     return data.isFavorite === true;
   });
-  return (
-    <>
-      <h1>즐겨찾기</h1>
 
-      <div>
-        {favoritedDustData?.map((data) => {
-          const {
-            sidoName,
-            stationName,
-            pm10Grade,
-            pm10Value,
-            dataTime,
-            isFavorite,
-          } = data;
-          return (
-            <Card
-              key={stationName}
-              sidoName={sidoName}
-              stationName={stationName}
-              pm10Grade={pm10Grade}
-              pm10Value={pm10Value}
-              dataTime={dataTime}
-              isFavorite={isFavorite}
-            />
-          );
-        })}
+  if (favoritedDustData.length === 0) {
+    return (
+      <div className="text-[30px] font-[rotobo] font-bold text-center">
+        즐겨찾기 등록된 지역이 없습니다
       </div>
-    </>
+    );
+  }
+  return (
+    <div>
+      {favoritedDustData?.map((data) => {
+        const {
+          sidoName,
+          stationName,
+          pm10Grade,
+          pm10Value,
+          dataTime,
+          isFavorite,
+        } = data;
+        return (
+          <Card
+            key={stationName}
+            sidoName={sidoName}
+            stationName={stationName}
+            pm10Grade={pm10Grade}
+            pm10Value={pm10Value}
+            dataTime={dataTime}
+            isFavorite={isFavorite}
+          />
+        );
+      })}
+    </div>
   );
 }
 
