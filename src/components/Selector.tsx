891f2b720f0d92765a6d20e7aa3b565617db919d
hoona1011 <hoona1011@gmail.com>
2022-12-09 15:35:47 +0900

    feat: 셀렉트 컴포넌트 구현

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-function Selector() {
-  return <div>Selector</div>;
+interface SelectorProps {
+  regions: string[];
+  region: string;
+  setRegion: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function Selector({ regions, region, setRegion }: SelectorProps) {
+  const onChangeHandler = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setRegion(e.target.value);
+    },
+    [setRegion]
+  );
+  return (
+    <div className="relative w-full lg:max-w-sm">
+      <select
+        value={region}
+        onChange={onChangeHandler}
+        className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+      >
+        <option>지역을 선택하세요</option>
+        {regions?.map((regionName: string) => (
+          <option key={regionName}>{regionName}</option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default Selector;
