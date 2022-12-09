fe7ec90c36f49a538379887c6abc3c43fc4b95ae
hoona1011 <hoona1011@gmail.com>
2022-12-09 20:11:00 +0900

    feat: 미세먼지 등급-색상 유틸 함수 구현

---
+++ src/utils/gradeToColorConverter.ts
@@ -0,0 +1,17 @@
+const gradeToColorConverter = (gradeNumber: string) => {
+  switch (gradeNumber) {
+    case '1':
+      return 'text-[blue]';
+    case '2':
+      return 'text-[green]';
+    case '3':
+      return 'text-[yellow]';
+    case '4':
+      return 'text-[red]';
+
+    default:
+      return 'text-[darkgray]';
+  }
+};
+
+export default gradeToColorConverter;
