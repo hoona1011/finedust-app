996c897cca43c805916a7f5d9f2af20281700415
hoona1011 <hoona1011@gmail.com>
2022-12-09 15:35:23 +0900

    feat: 전체 지역 페이지 구현

--- src/pages/AllRegions.tsx
+++ src/pages/AllRegions.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import Card from 'components/Card';
+import Selector from 'components/Selector';
+import sidoNames from 'constants/sidoNames';
+import { DustInfo } from 'store/slices/dustSlice';
 
-function AllRegions() {
-  return <div>AllRegions</div>;
+interface AllregionsProps {
+  sido: string;
+  setSido: React.Dispatch<React.SetStateAction<string>>;
+  dustData: DustInfo[];
+}
+function AllRegions({ sido, setSido, dustData }: AllregionsProps) {
+  return (
+    <>
+      <h1 className="pl-[40px] pt-[120px] pb-[136px] text-[64px] font-[rotobo]">
+        전체 지역 보기
+      </h1>
+      <Selector regions={sidoNames} region={sido} setRegion={setSido} />
+      <div>
+        {dustData?.map((data) => {
+          const {
+            sidoName,
+            stationName,
+            pm10Grade,
+            pm10Value,
+            dataTime,
+            isFavorite,
+          } = data;
+          return (
+            <Card
+              key={stationName}
+              sidoName={sidoName}
+              stationName={stationName}
+              pm10Grade={pm10Grade}
+              pm10Value={pm10Value}
+              dataTime={dataTime}
+              isFavorite={isFavorite}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default AllRegions;
