bf14d5527666d0699f883ec5323520a56d9d328a
hoona1011 <hoona1011@gmail.com>
2022-12-12 19:31:28 +0900

    conf: 프록시 경로 변경 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    open: true,
     proxy: {
       '/api': {
         target:
